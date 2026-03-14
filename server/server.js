5662013dab228314cd8425226be1bd47b93234a6
Vishal Pandey <vishalpandey49535@gmail.com>
2026-03-15 03:38:51 +0530

    Add user routes — register, login, get user data

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(cors());
 await connectDB()
 
 app.get("/", (req, res) => res.send("Server is live..."));
+app.use("/api/users", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
