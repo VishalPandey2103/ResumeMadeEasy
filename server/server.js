1bb302817162f126891f360b5a13eea97c205c81
Vishal Pandey <vishalpandey49535@gmail.com>
2026-03-15 22:59:22 +0530

    Resume Page Complete

--- server/server.js
+++ server/server.js
@@ -3,20 +3,20 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
 import userRouter from "./routes/userRoutes.js";
-
+import resumeRouter from "./routes/resumeRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+await connectDB();
+
 app.use(express.json());
 app.use(cors());
 
-// Database connection
-await connectDB()
-
-app.get("/", (req, res) => res.send("Server is live..."));
-app.use("/api/users", userRouter);
+app.get('/', (req, res) => res.send("Server is live..."));
+app.use('/api/users', userRouter);
+app.use('/api/resumes', resumeRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
