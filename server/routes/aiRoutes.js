366724d396bc2386049374c224469423c91f1aaf
Vishal Pandey <vishalpandey49535@gmail.com>
2026-04-17 11:50:30 +0530

    AI route rate limit added

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -2,12 +2,13 @@ import express from "express";
 import protect from "../middlewares/authMiddleware.js";
 // addtion of new controller for getting ATS score
 import { enhanceJobDescription, enhanceProfessionalSummary, uploadResume, getATSScore } from '../controllers/aiController.js';
+import { aiLimiter } from "../middlewares/rateLimiter.js";
 
 const aiRouter = express.Router();
 
-aiRouter.post('/enhance-pro-sum', protect, enhanceProfessionalSummary)
-aiRouter.post('/ats-score', protect, getATSScore)
-aiRouter.post('/enhance-job-desc', protect, enhanceJobDescription)
-aiRouter.post('/upload-resume', protect, uploadResume)
+aiRouter.post('/enhance-pro-sum', protect, aiLimiter,enhanceProfessionalSummary)
+aiRouter.post('/ats-score', protect, aiLimiter,getATSScore)
+aiRouter.post('/enhance-job-desc', protect, aiLimiter, enhanceJobDescription)
+aiRouter.post('/upload-resume', protect, aiLimiter, uploadResume)
 
 export default aiRouter
